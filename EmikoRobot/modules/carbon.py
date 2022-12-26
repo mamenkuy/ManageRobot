615e9bdc21a3d5d16801485a32d50d2875de0a6e
mamenkuy <112274751+mamenkuy@users.noreply.github.com>
2022-12-26 22:43:22 +0700

    Update carbon.py

--- EmikoRobot/modules/carbon.py
+++ EmikoRobot/modules/carbon.py
@@ -46,10 +46,9 @@ async def repo(_, message):
                     InlineKeyboardButton(
                         "Repo", url="https://github.com/mamenkuy/ManageRobot"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/havingsexid")
+                        "Support", url=f"https://t.me/havingsexid")
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/skymusicku")
-
+                        "Channel", url=f"https://t.me/skymusicku")
                 ]
             ]
         )
